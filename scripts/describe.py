76f2cdc464173af5a2aad7e878e56d790ad3f1c3
Mihai Rotaru <mihai_rotaru@lavabit.com>
2012-03-22 19:26:02 +0000

    fix: on Win, executables end with .exe

--- scripts/describe.py
+++ scripts/describe.py
@@ -9,13 +9,12 @@
 import os
 from subprocess import *
 
+def is_exe(fpath):
+    return os.path.exists(fpath) and os.access(fpath, os.X_OK)
+
 # look for executable, and return full path
 # from: [ http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python ]
 def which(program):
-    import os
-    def is_exe(fpath):
-        return os.path.exists(fpath) and os.access(fpath, os.X_OK)
-
     fpath, fname = os.path.split(program)
     if fpath:
         if is_exe(program):
@@ -23,9 +22,8 @@ def which(program):
     else:
         for path in os.environ["PATH"].split(os.pathsep):
             exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
+            if is_exe(exe_file) or is_exe(exe_file+'.exe'):
                 return exe_file
-
     return None
 
 if not which( 'git' ):
